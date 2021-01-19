52f9a07b54a997ef87304e489195299cd62cf550
David C <dcrouseIII@hotmail.com>
2021-01-19 00:10:42 -0500

    Add reload command

--- src/cogs/strawpoll.py
+++ src/cogs/strawpoll.py
@@ -1,9 +1,22 @@
 import discord
+import requests
+import pprint
 from discord.ext import commands
 
 class Strawpoll(commands.Cog):
+    url = 'https://www.strawpoll.me/api/v2/polls'
+    pp = pprint.PrettyPrinter(depth=6)
+
     def __init__(self, movie_bot):
         self.movie_bot = movie_bot
+
+    @commands.command()
+    async def makepoll(self, context):
+        # data = { "title": "This is a test poll.", "options": ["Option #1", "Option #2"], "multi": True }
+        # response = requests.post(url=self.url, json=data)
+        # data = response.json()
+        # print(data)
+        return
 
 def setup(movie_bot):
     movie_bot.add_cog(Strawpoll(movie_bot))
--- src/movie_bot.py
+++ src/movie_bot.py
@@ -9,14 +9,35 @@ movie_bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print('Logged on as ' + Fore.BLUE + str(movie_bot.user) + Fore.WHITE)
 
+@movie_bot.command(hidden=True)
+async def reload(context):
+    if str(context.message.author) in admins:
+        reload_cogs()
+        await context.channel.purge(limit=1)
+    else:
+        await context.channel.purge(limit=1)
+        await context.channel.send(context.message.author.mention + ' you do not have access to this command.')
+
+def load_cogs():
+    for filename in os.listdir('src\\cogs'):
+        if filename.endswith('.py'):
+            print('Loading ' + Fore.YELLOW + filename[:-3] + Fore.WHITE + ' cog')
+            movie_bot.load_extension(f'cogs.{filename[:-3]}')
+
+def reload_cogs():
+    for filename in os.listdir('src\\cogs'):
+        if filename.endswith('.py'):
+            print('Reloading ' + Fore.YELLOW + filename[:-3] + Fore.WHITE + ' cog')
+            movie_bot.reload_extension(f'cogs.{filename[:-3]}')
+
 # setup and login
 authfile = open('src\\auth.txt','r')
 auth = authfile.read()
 authfile.close()
 
-for filename in os.listdir('src\\cogs'):
-    if filename.endswith('.py'):
-        print('Loading ' + Fore.YELLOW + filename[:-3] + Fore.WHITE + ' cog')
-        movie_bot.load_extension(f'cogs.{filename[:-3]}')
+adminfile = open('src\\admins.txt','r')
+admins = adminfile.read().split(',')
+adminfile.close()
 
+load_cogs()
 movie_bot.run(auth)
