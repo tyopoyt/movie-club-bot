abec5fbfc106811a26c403342e5152895a1555d6
David C <dcrouseIII@hotmail.com>
2021-01-21 00:59:03 -0500

    Add easter egg command for thomas

--- src/cogs/utils.py
+++ src/cogs/utils.py
@@ -2,6 +2,9 @@ import discord
 from datetime import datetime
 from discord.ext import commands
 
+def setup(movie_bot):
+    movie_bot.add_cog(Utilities(movie_bot))
+
 class Utilities(commands.Cog):
     movie_bot = None
 
@@ -29,5 +32,6 @@ class Utilities(commands.Cog):
         except:
             await context.channel.send(f'\"{amount}\" is not a number')
 
-def setup(movie_bot):
-    movie_bot.add_cog(Utilities(movie_bot))
+    @commands.command(hidden=True)
+    async def thomas(self, context):
+        await context.channel.send("**Quoth White Thomas,** \"*I guess you forgot about the time you and Bravo company left my white ass for dead, huh? But I remember. I remember everything. I remember Vietnam like it was yesterday. I remember that village in Tainan that we cut down. It was a massacre. All the dead Chinamen we left in our tracks. I remember the faces, the children. This one child I'll never forget. Poor little bastard was still alive. His little Chinese legs were blown clean off! Still see his little shins & feet hanging from the ceiling fan across the hut. He was charred from his head down to his little Chinese knees. He tried to get up, but he fell over when what was left of his right leg broke off. As he laid there, flat on his face, he looked up at me. His little Chinese eyes burned right into my stomach, deep into my soul. He said something to me in Chinese like, 'Boo coo sow!', sounded like some cartoon shit. But I understood it to be a question that he was asking me. And I don't have to know how to speak Chinese to know what that question was. 'Why, White Thomas? Why?'* \"")
